6a606c3621d8c25cb220d48ed0c279d8b0ed1d7c
Zhi-Wei <ZhiWei.Lu@outlook.com>
2018-10-20 22:54:02 +0800

    新建 HttpUtilityUrlEncodeTests HttpUtility 類別 URL 編碼的測試類別。

---
+++ UrlEncodingAnalysisTests/DotNetFramework/HttpUtilityUrlEncodeTests.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UrlEncodingAnalysisTests.DotNetFramework
+{
+    /// <summary>
+    /// HttpUtility 類別 URL 編碼的測試類別。
+    /// </summary>
+    [TestClass]
+    public class HttpUtilityUrlEncodeTests
+    {
+    }
+}
