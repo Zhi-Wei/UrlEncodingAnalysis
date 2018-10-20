9e8cc5d7c349357d31d0ea86ffd02a5df8b7753a
Zhi-Wei <ZhiWei.Lu@outlook.com>
2018-10-20 22:38:49 +0800

    新建 UrlEncodingAnalysisTests 測試專案。

---
+++ UrlEncodingAnalysisTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("UrlEncodingAnalysisTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("UrlEncodingAnalysisTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("053b20ac-2b2d-45ce-8890-d4c8b487a1ab")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
