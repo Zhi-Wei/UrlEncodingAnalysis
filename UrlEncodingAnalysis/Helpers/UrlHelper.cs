7f59adeb75fa1853698ab9525021383f5503f1da
Zhi-Wei <ZhiWei.Lu@outlook.com>
2018-12-09 15:53:57 +0800

    新建 UrlHelper 輔助程式類別，並實作 URL 編解碼方法。

---
+++ UrlEncodingAnalysis/Helpers/UrlHelper.cs
@@ -0,0 +1,72 @@
+﻿using System.Linq;
+using System.Text;
+using System.Text.Encodings.Web;
+using Flurl;
+
+namespace UrlEncodingAnalysis.Helpers
+{
+    /// <summary>
+    /// URL 輔助程式。
+    /// </summary>
+    public class UrlHelper
+    {
+        /// <summary>
+        /// High Surrogate 字元集合。
+        /// </summary>
+        private static readonly char[] HighSurrogateChars = GenerateHighSurrogateChars();
+
+        /// <summary>
+        /// 產生 High Surrogate 字元集合。
+        /// </summary>
+        /// <returns>High Surrogate 字元集合。</returns>
+        private static char[] GenerateHighSurrogateChars()
+        {
+            int highSurrogateStart = 0xD800;
+            int highSurrogateEnd = 0xDBFF;
+            int highSurrogateCount = highSurrogateEnd - highSurrogateStart + 1;
+            var highSurrogateChars =
+                Enumerable.Range(highSurrogateStart, highSurrogateCount).Select(i => (char)i).ToArray();
+
+            return highSurrogateChars;
+        }
+
+        /// <summary>
+        /// URL 編碼。
+        /// </summary>
+        /// <param name="url">欲編碼的 URL。</param>
+        /// <returns>編碼後的 URL。</returns>
+        public static string UrlEncode(string url)
+        {
+            if (url == null)
+            {
+                return url;
+            }
+
+            var builder = new StringBuilder();
+
+            foreach (var item in url)
+            {
+                if (HighSurrogateChars.Contains(item))
+                {
+                    builder.Append(UrlEncoder.Default.Encode(item.ToString()));
+
+                    continue;
+                }
+
+                builder.Append(Url.Encode(item.ToString()));
+            }
+
+            return builder.ToString();
+        }
+
+        /// <summary>
+        /// URL 解碼。
+        /// </summary>
+        /// <param name="encodedUrl">編碼後的 URL。</param>
+        /// <returns>解碼後的 URL。</returns>
+        public static string UrlDecode(string encodedUrl)
+        {
+            return Url.Decode(encodedUrl, false);
+        }
+    }
+}
