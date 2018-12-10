5942c8ecabb7653599d3e25ecac92e0ae3b6ad1f
Zhi-Wei <ZhiWei.Lu@outlook.com>
2018-12-10 13:27:12 +0800

    調整 UrlHelper.UrlEncode 方法的實作。

--- UrlEncodingAnalysis/Helpers/UrlHelper.cs
+++ UrlEncodingAnalysis/Helpers/UrlHelper.cs
@@ -11,26 +11,6 @@ namespace UrlEncodingAnalysis.Helpers
     public class UrlHelper
     {
         /// <summary>
-        /// High Surrogate 字元集合。
-        /// </summary>
-        private static readonly char[] HighSurrogateChars = GenerateHighSurrogateChars();
-
-        /// <summary>
-        /// 產生 High Surrogate 字元集合。
-        /// </summary>
-        /// <returns>High Surrogate 字元集合。</returns>
-        private static char[] GenerateHighSurrogateChars()
-        {
-            int highSurrogateStart = 0xD800;
-            int highSurrogateEnd = 0xDBFF;
-            int highSurrogateCount = highSurrogateEnd - highSurrogateStart + 1;
-            var highSurrogateChars =
-                Enumerable.Range(highSurrogateStart, highSurrogateCount).Select(i => (char)i).ToArray();
-
-            return highSurrogateChars;
-        }
-
-        /// <summary>
         /// URL 編碼。
         /// </summary>
         /// <param name="url">欲編碼的 URL。</param>
@@ -46,7 +26,7 @@ namespace UrlEncodingAnalysis.Helpers
 
             foreach (var item in url)
             {
-                if (HighSurrogateChars.Contains(item))
+                if (char.IsHighSurrogate(item))
                 {
                     builder.Append(UrlEncoder.Default.Encode(item.ToString()));
 
